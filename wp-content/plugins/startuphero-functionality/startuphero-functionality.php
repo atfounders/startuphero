11f2d4f43b1f6c07b83a7b52d1aca309a1a36597
Ryan Imel <ryan@aspiringindie.com>
2012-12-22 12:19:35 -0500

    Added metabox class to plugin, started with a field for link formats.

--- wp-content/plugins/startuphero-functionality/startuphero-functionality.php
+++ wp-content/plugins/startuphero-functionality/startuphero-functionality.php
@@ -13,3 +13,42 @@ add_theme_support(
 	array( 
 		'link' ) 
 );
+
+// Metabox class inclusion
+add_action( 'init', 'startuphero_metabox_initialize', 9999 );
+
+function startuphero_metabox_initialize() {
+
+	if ( ! class_exists( 'cmb_Meta_Box' ) )
+		require_once 'metabox/init.php';
+
+}
+
+// Create our metaboxes
+add_filter( 'cmb_meta_boxes', 'startuphero_metabox_create' );
+
+function startuphero_metabox_create( array $meta_boxes ) {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_startuphero_';
+
+	$meta_boxes[] = array(
+		'id'         => 'startuphero_additional_info',
+		'title'      => 'Additional Info',
+		'pages'      => array( 'post' ), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+		'show_names' => true, // Show field names on the left
+		'fields'     => array(
+			array(
+				'name' => 'Link Format URL',
+				'desc' => 'Include the full URL with <code>http://</code>.',
+				'id'   => $prefix . 'post_link_url',
+				'type' => 'text',
+			),
+		),
+	);
+
+	return $meta_boxes;
+}
+
