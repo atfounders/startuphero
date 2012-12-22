61fab87d4a9a63ace5a1eba5c8c048b30463e140
Ryan Imel <ryan@aspiringindie.com>
2012-12-22 11:38:56 -0500

    Added functionality plugin with post formats enabled.

---
+++ wp-content/plugins/startuphero-functionality/startuphero-functionality.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Startup Hero Functionality
+Description: Special plugin just for StartupHero.com.
+Version: 1.0
+Author: Ryan Imel
+License: GPLv2 or later
+*/
+
+// Enable post formats
+add_theme_support( 
+	'post-formats', 
+	array( 
+		'link' ) 
+);
