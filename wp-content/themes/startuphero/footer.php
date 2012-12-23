7b26c06bcfa077113015326134704f7e1f617d18
Ryan Imel <ryan@aspiringindie.com>
2012-12-23 11:50:11 -0500

    Added credit to the footer.

--- wp-content/themes/startuphero/footer.php
+++ wp-content/themes/startuphero/footer.php
@@ -16,6 +16,8 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
 				<?php do_action( 'confit_credits' ); ?>
+				<a href="http://atfounders.com" title="A Founders Project">A Founders Project</a>
+				<span class="sep"> | </span>
 				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'confit' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'confit' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
 				<?php printf( __( 'Theme: %1$s by %2$s.', 'confit' ), 'Confit', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
