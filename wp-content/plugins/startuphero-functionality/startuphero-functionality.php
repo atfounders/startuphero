8d79e0af1d9e8a25fabd6986d95399e0107dd4b8
Ryan Imel <ryan@aspiringindie.com>
2012-12-24 11:59:43 -0500

    Updated WordPress SEO, removed dashboard widgets we don't need.

--- wp-content/plugins/startuphero-functionality/startuphero-functionality.php
+++ wp-content/plugins/startuphero-functionality/startuphero-functionality.php
@@ -52,6 +52,19 @@ function startuphero_metabox_create( array $meta_boxes ) {
 	return $meta_boxes;
 }
 
+// Remove dashboard widgets
+add_action( 'wp_dashboard_setup', 'startuphero_remove_dashboard_widgets' );
+
+function startuphero_remove_dashboard_widgets() {
+	remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
+}
+
 // Enable HTML in widget titles.
 add_filter( 'widget_title', 'startuphero_html_in_widget_titles' );
 
